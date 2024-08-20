726945b4c114b10751d0ca10a483dd316448481f
devwunmi <olayiwolapamilerin34@gmail.com>
2024-08-20 13:08:03 +0100

    chore: Remove password from user schema; add new user file in controller folder

--- models/user.ts
+++ models/user.ts
@@ -1,14 +1,12 @@
-import mongoose, { Schema } from 'mongoose';
-import bcrypt from 'bcrypt';
+import mongoose, { Schema } from "mongoose";
 
 const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   isEmailVerified: { type: Boolean, default: false },
-  password: { type: String, required: true },
   otp: { type: Number, unique: true },
-  userType: { type: String, default: 'User', enum: ['User', 'Admin'] },
+  userType: { type: String, default: "User", enum: ["User", "Admin"] },
   mobileNumber: { type: String, required: true },
   placeOfResidence: { type: String, required: true },
   socialMediaPlatform: { type: String, required: true },
@@ -16,15 +14,4 @@ const UserSchema: Schema = new Schema({
   howDidYouFindOut: { type: [String], required: true },
 });
 
-UserSchema.pre("save", function (next) {
-  const user = this;
-
-  bcrypt.hash(user.password as string, 10, (err, hash) => {
-    if (err) return next(err);
-    user.password = hash;
-    next();
-  });
-});
-
-
-export const UserModel = mongoose.model('User', UserSchema);
+export const UserModel = mongoose.model("User", UserSchema);
