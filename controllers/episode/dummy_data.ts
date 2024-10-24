c6882d65661360706fd321f1d5c33d1c5d9a0c83
deverins <olayiwolapamilerin34@gmail.com>
2024-10-24 12:55:26 +0100

    Remove dummy_data.ts from repo and add to .gitignore

---
+++ controllers/episode/dummy_data.ts
@@ -0,0 +1,364 @@
+export const data = [
+  {
+    "episode": 20,
+    "participantName": "Akinboboye Rotimi",
+    "amountAvailabeTowin": 1000000,
+    "amountWon": 5000,
+    "link": "https://youtu.be/eWOLnpS0Xok?si=XNgerakNF4J-oKJ4",
+    "date": "May 24, 2024",
+    "events": [
+      {
+        "type": "CODE_MIX",
+        "question": "",
+        "expectedAnswer": "",
+        "response": "High",
+        "isCorrect": false,
+        "amount": -50000,
+        "balance": 50000
+      },
+      {
+        "type": "CODE_MIX",
+        "question": "",
+        "expectedAnswer": "",
+        "response": "Chief",
+        "isCorrect": false,
+        "amount": -250000,
+        "balance": 250000
+      },
+      {
+        "type": "QUESTION_NUMBER",
+        "question": "99",
+        "expectedAnswer": "Ibere Kokandilogorun",
+        "response": "Ibere Kokandilogorun",
+        "isCorrect": true,
+        "amount": 0,
+        "balance": 250000
+      },
+      {
+        "type": "QUESTION",
+        "question": "pari owe yii \"Sin mi ka re ile ana o wo ewu etu...\"",
+        "expectedAnswer": "Kini ki oko iyawo wo",
+        "response": "",
+        "isCorrect": false,
+        "amount": -100000,
+        "balance": 100000
+      },
+      {
+        "type": "CODE_MIX",
+        "question": "",
+        "expectedAnswer": "",
+        "response": "Okay",
+        "isCorrect": false,
+        "amount": -50000,
+        "balance": 50000
+      },
+      {
+        "type": "QUESTION_NUMBER",
+        "question": "87",
+        "expectedAnswer": "Ibere Ketadinlaadorun",
+        "response": "Orinle Ketadinlaadorun",
+        "isCorrect": true,
+        "amount": 0,
+        "balance": 50000
+      },
+      {
+        "type": "QUESTION",
+        "question": "What is the yoruba name of the animal \"deer?\"",
+        "expectedAnswer": "Agbonrin",
+        "response": "",
+        "isCorrect": false,
+        "amount": -10000,
+        "balance": 40000
+      },
+      {
+        "type": "QUESTION_NUMBER",
+        "question": "88",
+        "expectedAnswer": "Ibere Kejidinlaadorun",
+        "response": "Ibere Kejidinlaadorun",
+        "isCorrect": true,
+        "amount": 0,
+        "balance": 35000
+      },
+      {
+        "type": "QUESTION",
+        "question": "What is \"fountain\" in yoruba?",
+        "expectedAnswer": "Orisun",
+        "response": "Orisun",
+        "isCorrect": true,
+        "amount": 0,
+        "balance": 35000
+      },
+      {
+        "type": "QUESTION_NUMBER",
+        "question": "86",
+        "expectedAnswer": "Ibere Kerindinlaadorun",
+        "response": "Erindinlaadorin",
+        "isCorrect": false,
+        "amount": -5000,
+        "balance": 30000
+      },
+      {
+        "type": "QUESTION",
+        "question": "What do you call the money you paid as \"fine\" in yoruba",
+        "expectedAnswer": "Owo itanran",
+        "response": "Owo itanran",
+        "isCorrect": true,
+        "amount": 0,
+        "balance": 30000
+      },
+      {
+        "type": "QUESTION",
+        "question": "Daruko iye ti owo yin ku ",
+        "expectedAnswer": "egberun ni ona ogbon",
+        "response": "Ogbon ku",
+        "isCorrect": false,
+        "amount": -5000,
+        "balance": 25000
+      },
+      {
+        "type": "CODE_MIX",
+        "question": "",
+        "expectedAnswer": "",
+        "response": "Okay",
+        "isCorrect": false,
+        "amount": -5000,
+        "balance": 20000
+      },
+      {
+        "type": "QUESTION_NUMBER",
+        "question": "111",
+        "expectedAnswer": "Ibere Ikokanlelaadofa",
+        "response": "Ibere Ikokanlelogorun",
+        "isCorrect": false,
+        "amount": -5000,
+        "balance": 15000
+      },
+      {
+        "type": "QUESTION",
+        "question": "Ere ori igi aseku omo edun kini yoruba n pe ni \"edun\"?",
+        "expectedAnswer": "obo",
+        "response": "obo",
+        "isCorrect": true,
+        "amount": 0,
+        "balance": 15000
+      },
+      {
+        "type": "QUESTION_NUMBER",
+        "question": "100",
+        "expectedAnswer": "Ibere ogorun",
+        "response": "Ibere ogorun",
+        "isCorrect": true,
+        "amount": 0,
+        "balance": 15000
+      },
+      {
+        "type": "QUESTION",
+        "question": "Pari owe yii yoruba bo won ni... Pansa o fura pansa ja, aja o fura aja jin ..ki ni pansa?",
+        "expectedAnswer": "Ate ti won ko ikan si loke aja",
+        "response": "",
+        "isCorrect": false,
+        "amount": -5000,
+        "balance": 10000
+      },
+      {
+        "type": "QUESTION_NUMBER",
+        "question": "115",
+        "expectedAnswer": "Ibere Karundinlogofa",
+        "response": "Ibere Karundinlogofa",
+        "isCorrect": true,
+        "amount": 0,
+        "balance": 100000
+      },
+      {
+        "type": "QUESTION",
+        "question": "Recite the Nigeria national pledge in yoruba",
+        "expectedAnswer": "Mo je eje fun orilede mi nigeria lati je olotito ati olododo lat fi gbogbo agbara sin orilede nigeria lati ma ja fun isokan re at lati gbe iyin ati ogo re ga ki oluwa ki oluwa ki ran mi lowo",
+        "response": "Mo je eje fun orilede mi nigeria lati je olotito ati olododo",
+        "isCorrect":  false,
+        "amount": -5000,
+        "balance": 5000
+      },
+    ]
+  },
+  // {
+  //   "episode": 21,
+  //   "participantName": "Akinboboye Rotimi",
+  //   "amountAvailabeTowin": 1000000,
+  //   "amountWon": 5000,
+  //   "link": "https://youtu.be/eWOLnpS0Xok?si=XNgerakNF4J-oKJ4",
+  //   "date": "May 24, 2024",
+  //   "events": [
+  //     {
+  //       "type": "CODE_MIX",
+  //       "question": "",
+  //       "expectedAnswer": "",
+  //       "response": "High",
+  //       "isCorrect": false,
+  //       "amount": -50000,
+  //       "balance": 50000
+  //     },
+  //     {
+  //       "type": "CODE_MIX",
+  //       "question": "",
+  //       "expectedAnswer": "",
+  //       "response": "Chief",
+  //       "isCorrect": false,
+  //       "amount": -250000,
+  //       "balance": 250000
+  //     },
+  //     {
+  //       "type": "QUESTION_NUMBER",
+  //       "question": "99",
+  //       "expectedAnswer": "Ibere Kokandilogorun",
+  //       "response": "Ibere Kokandilogorun",
+  //       "isCorrect": true,
+  //       "amount": 0,
+  //       "balance": 250000
+  //     },
+  //     {
+  //       "type": "QUESTION",
+  //       "question": "pari owe yii \"Sin mi ka re ile ana o wo ewu etu...\"",
+  //       "expectedAnswer": "Kini ki oko iyawo wo",
+  //       "response": "",
+  //       "isCorrect": false,
+  //       "amount": -100000,
+  //       "balance": 100000
+  //     },
+  //     {
+  //       "type": "CODE_MIX",
+  //       "question": "",
+  //       "expectedAnswer": "",
+  //       "response": "Okay",
+  //       "isCorrect": false,
+  //       "amount": -50000,
+  //       "balance": 50000
+  //     },
+  //     {
+  //       "type": "QUESTION_NUMBER",
+  //       "question": "87",
+  //       "expectedAnswer": "Ibere Ketadinlaadorun",
+  //       "response": "Orinle Ketadinlaadorun",
+  //       "isCorrect": true,
+  //       "amount": 0,
+  //       "balance": 50000
+  //     },
+  //     {
+  //       "type": "QUESTION",
+  //       "question": "What is the yoruba name of the animal \"deer?\"",
+  //       "expectedAnswer": "Agbonrin",
+  //       "response": "",
+  //       "isCorrect": false,
+  //       "amount": -10000,
+  //       "balance": 40000
+  //     },
+  //     {
+  //       "type": "QUESTION_NUMBER",
+  //       "question": "88",
+  //       "expectedAnswer": "Ibere Kejidinlaadorun",
+  //       "response": "Ibere Kejidinlaadorun",
+  //       "isCorrect": true,
+  //       "amount": 0,
+  //       "balance": 35000
+  //     },
+  //     {
+  //       "type": "QUESTION",
+  //       "question": "What is \"fountain\" in yoruba?",
+  //       "expectedAnswer": "Orisun",
+  //       "response": "Orisun",
+  //       "isCorrect": true,
+  //       "amount": 0,
+  //       "balance": 35000
+  //     },
+  //     {
+  //       "type": "QUESTION_NUMBER",
+  //       "question": "86",
+  //       "expectedAnswer": "Ibere Kerindinlaadorun",
+  //       "response": "Erindinlaadorin",
+  //       "isCorrect": false,
+  //       "amount": -5000,
+  //       "balance": 30000
+  //     },
+  //     {
+  //       "type": "QUESTION",
+  //       "question": "What do you call the money you paid as \"fine\" in yoruba",
+  //       "expectedAnswer": "Owo itanran",
+  //       "response": "Owo itanran",
+  //       "isCorrect": true,
+  //       "amount": 0,
+  //       "balance": 30000
+  //     },
+  //     {
+  //       "type": "QUESTION",
+  //       "question": "Daruko iye ti owo yin ku ",
+  //       "expectedAnswer": "egberun ni ona ogbon",
+  //       "response": "Ogbon ku",
+  //       "isCorrect": false,
+  //       "amount": -5000,
+  //       "balance": 25000
+  //     },
+  //     {
+  //       "type": "CODE_MIX",
+  //       "question": "",
+  //       "expectedAnswer": "",
+  //       "response": "Okay",
+  //       "isCorrect": false,
+  //       "amount": -5000,
+  //       "balance": 20000
+  //     },
+  //     {
+  //       "type": "QUESTION_NUMBER",
+  //       "question": "111",
+  //       "expectedAnswer": "Ibere Ikokanlelaadofa",
+  //       "response": "Ibere Ikokanlelogorun",
+  //       "isCorrect": false,
+  //       "amount": -5000,
+  //       "balance": 15000
+  //     },
+  //     {
+  //       "type": "QUESTION",
+  //       "question": "Ere ori igi aseku omo edun kini yoruba n pe ni \"edun\"?",
+  //       "expectedAnswer": "obo",
+  //       "response": "obo",
+  //       "isCorrect": true,
+  //       "amount": 0,
+  //       "balance": 15000
+  //     },
+  //     {
+  //       "type": "QUESTION_NUMBER",
+  //       "question": "100",
+  //       "expectedAnswer": "Ibere ogorun",
+  //       "response": "Ibere ogorun",
+  //       "isCorrect": true,
+  //       "amount": 0,
+  //       "balance": 15000
+  //     },
+  //     {
+  //       "type": "QUESTION",
+  //       "question": "Pari owe yii yoruba bo won ni... Pansa o fura pansa ja, aja o fura aja jin ..ki ni pansa?",
+  //       "expectedAnswer": "Ate ti won ko ikan si loke aja",
+  //       "response": "",
+  //       "isCorrect": false,
+  //       "amount": -5000,
+  //       "balance": 10000
+  //     },
+  //     {
+  //       "type": "QUESTION_NUMBER",
+  //       "question": "115",
+  //       "expectedAnswer": "Ibere Karundinlogofa",
+  //       "response": "Ibere Karundinlogofa",
+  //       "isCorrect": true,
+  //       "amount": 0,
+  //       "balance": 100000
+  //     },
+  //     {
+  //       "type": "QUESTION",
+  //       "question": "Recite the Nigeria national pledge in yoruba",
+  //       "expectedAnswer": "Mo je eje fun orilede mi nigeria lati je olotito ati olododo lat fi gbogbo agbara sin orilede nigeria lati ma ja fun isokan re at lati gbe iyin ati ogo re ga ki oluwa ki oluwa ki ran mi lowo",
+  //       "response": "Mo je eje fun orilede mi nigeria lati je olotito ati olododo",
+  //       "isCorrect":  false,
+  //       "amount": -5000,
+  //       "balance": 5000
+  //     },
+  //   ]
+  // },
+]
